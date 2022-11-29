4adcf897ece67d29cccb128fd358c96e4d342431
go-go-go-Anay <96417407+go-go-go-Anay@users.noreply.github.com>
2022-11-29 17:42:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song = "";
+
+function preload(){
+song = loadSound("All my friends are toxic.mp3")
+}
+
+function setup(){
+canvas = createCanvas(600, 500);
+canvas.position(468, 200);
+
+video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+
+    image(video, 0, 0, 600, 500);
+
+}
+
+function play(){
+    song.play();
+}
